0db4d1dee579e62ee002f8db4da71e3c73bcd82e
Juan Jesus Tineo <jtineo@cash-converters.es>
2020-05-31 17:31:00 +0200

    add auth to requests in angular

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private API_URL = 'http://localhost:3000/users/';
+  private API_URL = `${environment.api_url}users/`;
+  private httpOptions = {
+    headers: (new HttpHeaders())
+      .append('Content-Type', 'application/json')
+      .append('Authorization', `Basic ${btoa(`${environment.user}:${environment.pass}`)}`)
+  };
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
 
   public getUsers() {
-    return this.http.post(`${this.API_URL}`, {});
+    return this.http.post(`${this.API_URL}`, {}, this.httpOptions);
   }
 }
