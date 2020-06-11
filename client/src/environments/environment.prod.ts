3036b7617aadfaa1e397f7e97862a4ea32bc10df
Juan Jesus Tineo <jtineo@cash-converters.es>
2020-06-11 18:11:11 +0200

    *

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'http://localhost:3000/',
+  pass: 'devwebjobs9977',
+  user: 'devwebjobs',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDRN3HOHgKeAq19L5I6uA40aDlDqUgFLss',
+    authDomain: 'webjobs-5f938.firebaseapp.com',
+    databaseURL: 'https://webjobs-5f938.firebaseio.com',
+    projectId: 'webjobs-5f938',
+    storageBucket: 'webjobs-5f938.appspot.com',
+    messagingSenderId: '17218087289',
+    appId: '1:17218087289:web:8198fb3bc70d8c29aea7a8'
+  }
 };
